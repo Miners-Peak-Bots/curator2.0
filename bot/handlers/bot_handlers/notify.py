fea6ff0b296b7d9db97ff2491a5ac2d75cb941a4
Haider <me@haiderali.dev>
2022-06-29 18:07:04 +0530

    update notify handler

--- bot/handlers/bot_handlers/notify.py
+++ bot/handlers/bot_handlers/notify.py
@@ -1,32 +1,36 @@
 from ...utils.msg import errorify
 from pyrogram.handlers import MessageHandler
 from pyrogram import filters
+from group.models import Group
+from django.conf import settings
 
 
 __HELP__ = """Hey man
 How is it going"""
 
 
-async def handle_notify(client, msg):
-    if msg.from_user.id != client.config.general.master_id:
-        await msg.delete()
+def handle_notify(client, msg):
+    print('i am running')
+    if msg.from_user.id != settings.BOT_MASTER:
+        msg.delete()
         return False
 
-    message = msg.text.replace('!notification', '').strip()
-    if not len(message) > 0:
-        await msg.reply_text('Please provide a valid message')
+    if not msg.reply_to_message:
+        msg.delete()
+        return False
 
+    message = msg.reply_to_message.text
     errors = []
-    for group in Group.select():
+    for group in Group.objects.all():
         try:
-            await client.send_message(group.group_id, message)
+            client.send_message(group.group_id, message)
         except Exception as e:
             errors.append(
                 f'Failed to notify {group.group_id} due to {str(e)}'
             )
 
     response = errorify('Done!', errors)
-    await msg.reply_text(response)
+    msg.reply_text(response)
 
 
 __HANDLERS__ = [
