c58adf357b46e0ef007deec0f83c4643fce4160f
Haider <me@haiderali.dev>
2022-06-29 18:29:28 +0530

    remove unnecessary modules

--- bot/db/cache.py
+++
@@ -1,6 +0,0 @@
-from ..utils import (user as user_utils,
-                     group as group_utils)
-
-users_cache = user_utils.load_users_cache()
-groups_cache = group_utils.load_groups_cache()
-misc = {}
