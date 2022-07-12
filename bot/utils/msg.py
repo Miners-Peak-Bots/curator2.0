67c8fc94ef66279ff0e13e1bb8a23983700d0335
Haider <me@haiderali.dev>
2022-07-13 03:47:10 +0530

    add log util function

--- bot/utils/msg.py
+++ bot/utils/msg.py
@@ -1,3 +1,8 @@
+from django.conf import settings
+from logzero import logger
+from pyrogram.enums import ParseMode
+
+
 def errorify(msg, erray):
     errors = f'Errors occured: {len(erray)}\n'
     errors = errors + '<code>' + '\n'.join(erray) + '</code>'
@@ -28,3 +33,14 @@ def titlefy_simple(key, value, nl=False):
 
 def boldify(phrase):
     return f'<b>{phrase}</b>'
+
+
+def log(client, message):
+    try:
+        client.send_message(
+            chat_id=settings.LOG_GROUP,
+            text=message,
+            parse_mode=ParseMode.HTML
+        )
+    except Exception as e:
+        logger.error(e)
