9c8a45d7dfc4808bcda9ea236429482e6460b787
Haider <me@haiderali.dev>
2022-10-21 21:11:39 +0530

    load blacklist into cache on startup

--- scripts/start_bot.py
+++ scripts/start_bot.py
@@ -1,5 +1,13 @@
 from bot import initialize
+from django.core.cache import cache
+from blacklist.models import Blacklist
 
 
 def run(*args):
+    """
+    Load the blacklist cache
+    """
+    query = Blacklist.objects.all()
+    blacklist = [row for row in query]
+    cache.set('blacklist', blacklist)
     initialize()
