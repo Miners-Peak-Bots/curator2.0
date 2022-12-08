0a828a6f9c2d9d4156c63bbd684c5f5d34d6b358
Haider <me@haiderali.dev>
2022-12-08 23:43:20 +0530

    blacklist is temp model change and migration

--- blacklist/models.py
+++ blacklist/models.py
@@ -5,6 +5,11 @@ import re
 
 class Blacklist(models.Model):
     regex = RegexField(max_length=128, re_flags=re.IGNORECASE)
+    """
+    This field when set to true will only be used to check against the users
+    first twenty messages
+    """
+    is_temp = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.regex)
