509ee39f1f06b452f1d2ab86e71ad1058d1be01f
Haider <me@haiderali.dev>
2022-12-09 20:36:50 +0530

    load blacklist20 into cache on startup

--- scripts/start_bot.py
+++ scripts/start_bot.py
@@ -7,7 +7,8 @@ def run(*args):
     """
     Load the blacklist cache
     """
-    query = Blacklist.objects.all()
-    blacklist = [row for row in query]
+    blacklist = [row for row in Blacklist.objects.filter(is_temp=False)]
+    blacklist20 = [row for row in Blacklist.objects.filter(is_temp=True)]
     cache.set('blacklist', blacklist)
+    cache.set('blacklist20', blacklist20)
     initialize()
