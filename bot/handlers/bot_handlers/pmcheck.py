bfc21bcd3be2d046df86b88a2732132d7f3e2a26
Haider <me@haiderali.dev>
2022-12-29 15:36:56 +0530

    reorganize

--- bot/handlers/bot_handlers/pmcheck.py
+++ bot/handlers/bot_handlers/pmcheck.py
@@ -1,77 +1,13 @@
-from ...utils.msg import titlefy, linkify, titlefy_simple, boldify
+from user.utils import (
+    prep_acheck,
+    prep_check
+)
 from pyrogram.handlers import MessageHandler
-from user.utils import create_get_user
 from pyrogram import filters
 from pyrogram.enums import ParseMode
-import emoji
 from user.models import (
     TeleUser
 )
-
-
-def prep_message_user(user):
-    text = f'User id: {user.tele_id}\n'
-    if user.first_name:
-        text = text + f'First name: {user.first_name}\n'
-    if user.username:
-        text = text + f'Username: @{user.username}\n'
-    if user.country:
-        country = emoji.emojize(f':{user.country}:'.title())
-        text = text + f'Country: {country}\n'
-    if user.verified:
-        text = text + emoji.emojize(':check_mark_button: Verified')
-
-    return text
-
-
-def prep_log(user):
-    if not user.logs.count():
-        return ''
-    response = []
-    logs = user.logs.all()
-    for log in logs:
-        response.append(
-            boldify(log.message)
-        )
-    response = '\n'.join(response)
-    return '<code>' + response + '</code>'
-
-
-def prep_message_admin(user):
-    text = '@MinersPeak <b>Curator Bot</b>\n--------------------\n'
-    text = text + titlefy('User id', user.tele_id)
-    text = text + titlefy('First name', user.first_name)
-    text = text + titlefy_simple('Username', user.username_tag)
-
-    country = None
-    if user.country:
-        country = emoji.emojize(f':{user.country}:'.title())
-    text = text + titlefy('Country', country)
-    text = (
-        text + linkify('keybase.io', 'keybase.io', True) +
-        ': ' + linkify(user.keybase_link, user.keybase) + '\n'
-    )
-    text = text + titlefy('Phone', user.ph_number, nl=True)
-    text = text + titlefy('Email', user.email, nl=True)
-    if user.verified:
-        text = text + '\n' + emoji.emojize(
-            ':check_mark_button: <code>Verified!</code>'
-        )
-    # text = text + '\n' + titlefy('Reputation', len(user.rep.select()))
-    text = text + '------------' + '\n'
-    warns = user.warning.count()
-    text = text + titlefy('Warns', f'{warns}/5')
-    banned_status = 'Yes' if user.banned else 'No'
-    text = text + titlefy('Banned', banned_status)
-
-    text = text + '------------' + '\n'
-    text = text + '<b>User log:\n</b>\n'
-    """
-    Add the logs
-    """
-    logs = prep_log(user)
-    text = text + logs
-    return text
 
 
 def pm_check(client, msg):
@@ -103,9 +39,9 @@ def pm_check(client, msg):
             return msg.reply('You do not have the rights to run this operation')
 
         if current_user.is_admin:
-            response = prep_message_admin(user)
+            response = prep_acheck(user)
         else:
-            response = prep_message_user(user)
+            response = prep_check(user)
 
         return client.send_message(
             chat_id=msg.from_user.id,
--- user/utils.py
+++ user/utils.py
@@ -1,3 +1,5 @@
+from bot.utils.msg import titlefy, linkify, titlefy_simple, boldify
+import emoji
 from .models import TeleUser
 from datetime import datetime, timedelta
 from pyrogram.types import ChatPermissions
@@ -54,3 +56,68 @@ def mute_user(client, chat_id, user_id, duration=30):
         permissions=ChatPermissions(),
         until_date=until
     )
+
+
+def prep_check(user):
+    text = f'User id: {user.tele_id}\n'
+    if user.first_name:
+        text = text + f'First name: {user.first_name}\n'
+    if user.username:
+        text = text + f'Username: @{user.username}\n'
+    if user.country:
+        country = emoji.emojize(f':{user.country}:'.title())
+        text = text + f'Country: {country}\n'
+    if user.verified:
+        text = text + emoji.emojize(':check_mark_button: Verified')
+
+    return text
+
+
+def prep_user_log(user):
+    if not user.logs.count():
+        return ''
+    response = []
+    logs = user.logs.all()
+    for log in logs:
+        response.append(
+            boldify(log.message)
+        )
+    response = '\n'.join(response)
+    return '<code>' + response + '</code>'
+
+
+def prep_acheck(user):
+    text = '@MinersPeak <b>Curator Bot</b>\n--------------------\n'
+    text = text + titlefy('User id', user.tele_id)
+    text = text + titlefy('First name', user.first_name)
+    text = text + titlefy_simple('Username', user.username_tag)
+
+    country = None
+    if user.country:
+        country = emoji.emojize(f':{user.country}:'.title())
+    text = text + titlefy('Country', country)
+    text = (
+        text + linkify('keybase.io', 'keybase.io', True) +
+        ': ' + linkify(user.keybase_link, user.keybase) + '\n'
+    )
+    text = text + titlefy('Phone', user.ph_number, nl=True)
+    text = text + titlefy('Email', user.email, nl=True)
+    if user.verified:
+        text = text + '\n' + emoji.emojize(
+            ':check_mark_button: <code>Verified!</code>'
+        )
+    # text = text + '\n' + titlefy('Reputation', len(user.rep.select()))
+    text = text + '------------' + '\n'
+    warns = user.warning.count()
+    text = text + titlefy('Warns', f'{warns}/5')
+    banned_status = 'Yes' if user.banned else 'No'
+    text = text + titlefy('Banned', banned_status)
+
+    text = text + '------------' + '\n'
+    text = text + '<b>User log:\n</b>\n'
+    """
+    Add the logs
+    """
+    logs = prep_user_log(user)
+    text = text + logs
+    return text
