02a6808e06375d708a3f3a4c5d022a0162615f22
Haider <me@haiderali.dev>
2023-01-01 00:45:15 +0530

    remove deprecated code from antispam module

--- bot/antispam.py
+++ bot/antispam.py
@@ -1,6 +1,5 @@
 from pyrogram import Client, idle, filters
 from django.conf import settings
-from blacklist.models import Blacklist
 from user.models import TeleUser
 from bot.utils.msg import log
 from pyrogram.enums import ChatType
@@ -12,21 +11,12 @@ token = settings.ANTISPAM_BOT_TOKEN
 
 app = Client('antispam.bot', api_id=api_id, api_hash=api_hash, bot_token=token)
 
-
-def get_patterns():
-    return Blacklist.objects.all()
-
+msgcount = {}
 
 def get_admins():
     admins_all = TeleUser.objects.filter(admin=True).values_list('tele_id')
     admins = [admin[0] for admin in admins_all] + [settings.BOT_MASTER]
     return admins
-
-
-data = {
-    'patterns': get_patterns(),
-    'admins': get_admins(),
-}
 
 
 @app.on_message(filters.regex('.+[\u4E00-\uA000]'))
@@ -39,18 +29,6 @@ def handle_msg2(client, msg):
     msg.delete()
 
 
-@app.on_message(filters.command('refreshbl'))
-def handle_refresh_bl(client, msg):
-    data['patterns'] = get_patterns()
-    msg.reply_text('Blacklist updated')
-
-
-@app.on_message(filters.command('refreshadmins'))
-def handle_refresh_admin(client, msg):
-    data['admins'] = get_admins()
-    msg.reply_text('Admins list updated')
-
-
 @app.on_message(filters.regex('(https?:\/\/)?(www[.])?(telegram|t)\.me\/([a-zA-Z0-9_-]*)\/?$'))
 def handle_msg3(client, msg):
     print('tele link detected')
@@ -59,13 +37,23 @@ def handle_msg3(client, msg):
 
 @app.on_message(filters.text)
 def handle_msg4(client, msg):
-    # user = msg.from_user.id
-    # if user in data['admins']:
-    #     return False
+    try:
+        current_count = msgcount[msg.from_user.id]
+    except KeyError:
+        current_count = 0
+
+    current_count = current_count + 1
+    msgcount[msg.from_user.id] = current_count
+
+    print(msgcount)
     admins = get_admins()
     if msg.from_user.id in admins:
         return False
 
+    if current_count <= 20:
+        print('user has less than 20 scanning')
+
+    patterns20 = cache.get('blacklist20', [])
     patterns = cache.get('blacklist', [])
     for pattern in patterns:
         res = pattern.regex.search(msg.text)
