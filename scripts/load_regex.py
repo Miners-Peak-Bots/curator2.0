edf2936b854005c3b9d165d423579769d7541cc1
Haider <me@haiderali.dev>
2023-01-04 02:01:27 +0530

    import blacklist from existing version

--- scripts/load_regex.py
+++ scripts/load_regex.py
@@ -3,13 +3,12 @@ from logzero import logger
 
 
 def run(**args):
-    with open('blacklist.txt') as f:
+    with open('regex_blacklist.txt') as f:
         lines = f.readlines()
         patterns = []
         for line in lines:
             phrase = line.strip()
-            phrase = f'\\b{phrase}\\b'
             patterns.append(Blacklist(regex=phrase))
 
         Blacklist.objects.bulk_create(patterns)
-        logger.info(f'Added {len(patterns)} patterns')
+        logger.info(f'Added {len(patterns)} regex patterns')
