5486dcbcf03a11b00f32d4fa1356fafdb9d2a899
Haider <me@haiderali.dev>
2023-01-06 18:06:50 +0530

    dont load group cfg cache on startup

--- scripts/start_bot.py
+++ scripts/start_bot.py
@@ -1,7 +1,7 @@
 from bot import initialize
 from django.core.cache import cache
 from blacklist.models import Blacklist
-from group.models import Group
+# from group.models import Group
 
 
 def run(*args):
@@ -10,11 +10,7 @@ def run(*args):
     """
     blacklist = [row for row in Blacklist.objects.filter(is_temp=False)]
     blacklist20 = [row for row in Blacklist.objects.filter(is_temp=True)]
-    group_config = {}
-    for group in Group.objects.all():
-        group_config[str(group.group_id)] = group.antispam
 
     cache.set('blacklist', blacklist)
     cache.set('blacklist20', blacklist20)
-    cache.set('group_cfg', group_config)
     initialize()
