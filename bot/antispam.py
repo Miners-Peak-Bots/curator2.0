e09597c65e3901f3e962b2a5b7ac64ad39dc7bb9
dheerajck <dheerajck18@gmail.com>
2024-06-01 23:03:09 +0530

    Add proper response if a users message have words or new line above group limit for that group

--- bot/antispam.py
+++ bot/antispam.py
@@ -142,23 +142,18 @@ def handle_msg5(client, msg):
         word_limit = group_limits.word_limit
         new_line_limit = group_limits.new_line_limit
 
-        if word_limit is not None:
-            if len(msg.text) > word_limit:
-                msg.delete()
-                return None
+        if word_limit is not None and len(msg.text) > word_limit:
+            msg.delete()
 
-        if new_line_limit is not None:
-            if msg.text.count('\n') > new_line_limit:
-                msg.delete()
-                return None
+        elif new_line_limit is not None and msg.text.count('\n') > new_line_limit:
+            msg.delete()
 
         sent = client.send_message(
             group_id,
-            text=f'Hi {msg.mention}, Make sure your message doesnt contain more than {word_limit} words(including spaces) and {new_line_limit} lines.',
+            text=f'Hi {msg.from_user.mention}, Make sure your message doesnt contain more than {word_limit} words(including spaces) and {new_line_limit} lines.',
         )
 
-        sched_cleanup(sent)
-
+        sched_cleanup(msg=sent, interval=10)
         return None
 
 
