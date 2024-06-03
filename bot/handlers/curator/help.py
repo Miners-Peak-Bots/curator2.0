0623bab561c335f54488051f0c29795b0958fe3c
dheerajck <dheerajck18@gmail.com>
2024-06-04 02:41:09 +0530

    Now $help/$commands will generate admin file even if the file is present, You arent saving any noticable time by not generating the file rn, so just generate it and change it when needed

--- bot/handlers/curator/help.py
+++ bot/handlers/curator/help.py
@@ -7,52 +7,41 @@ from user.models import (
 from django.conf import settings
 import os
 import textwrap
+
 CMD_PREFIX = settings.BOT_COMMAND_PREFIX
 
 
-def handle_help(client, msg):
-    response = '\n\n'.join(client.help)
-    response = f'<code>{response}</code>'
+def is_admin(user_id):
+    if user_id in settings.BOT_MASTER:
+        return True
 
-    is_admin = False
     try:
-        admin = TeleUser.objects.get(pk=msg.from_user.id)
-        is_admin = admin.is_admin
+        admin = TeleUser.objects.get(pk=user_id)
+        return admin.is_admin
     except TeleUser.DoesNotExist:
-        pass
+        return False
 
-    if is_admin:
+
+def handle_help(client, msg):
+    if is_admin(msg.from_user.id):
         help_ = client.help + client.admin_manual
+        help_text = "\n\n".join(help_)
+        with open("help-full.txt", "w") as f:
+            f.write(help_text)
+        client.send_document(chat_id=msg.chat.id, document="help-full.txt")
+
     else:
-        help_ = client.help
-
-    help_text = '\n\n'.join(help_)
-
-    if not is_admin:
-        response = f'<code>{response}</code>'
-        msg.reply(
-            text=response,
-            parse_mode=ParseMode.HTML
-        )
-    else:
-        if not os.path.isfile('help-full.txt'):
-            with open('help-full.txt', 'w') as f:
-                f.write(help_text)
-        client.send_document(
-            chat_id=msg.chat.id,
-            document='help-full.txt'
-        )
-
+        response = "\n\n".join(client.help)
+        response = f"<code>{response}</code>"
+        msg.reply(text=response, parse_mode=ParseMode.HTML)
 
 
 __HANDLERS__ = [
-    MessageHandler(handle_help,
-                   (filters.command(['help', 'commands'], prefixes=CMD_PREFIX)
-                    &
-                    filters.private))
+    MessageHandler(
+        handle_help,
+        (filters.command(["help", "commands"], prefixes=CMD_PREFIX) & filters.private),
+    )
 ]
 
 
-__HELP__ = (
-    '$help/$commands - Send list of all avialable commands and usage(in DM)'
-)
+__HELP__ = "$help/$commands - Send list of all avialable commands and usage(in DM)"
